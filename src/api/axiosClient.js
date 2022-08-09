5f97bb7d4998ca756c7a5dea7be944c5624e2eea
= <=>
2022-08-09 18:53:26 +0700

    config API

---
+++ src/api/axiosClient.js
@@ -0,0 +1,34 @@
+import axios from 'axios';
+import queryString from 'query-string';
+
+import apiConfig from './apiConfig';
+
+const axiosClient = axios.create({
+  baseURL: apiConfig.baseUrl,
+  headers: {
+    'content-type': 'application/json',
+  },
+  paramsSerializer: (params) => queryString.stringify({ ...params, api_key: apiConfig.apiKey }),
+});
+
+axiosClient.interceptors.request.use(
+  async (config) => {
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  },
+);
+
+axiosClient.interceptors.response.use(
+  (response) => {
+    if (response && response.data) return response.data;
+
+    return response;
+  },
+  (error) => {
+    return Promise.reject(error);
+  },
+);
+
+export default axiosClient;
---
+++ src/api/movieApi.js
@@ -0,0 +1,64 @@
+import axiosClient from './axiosClient';
+
+export const category = {
+  movie: 'movie',
+  tv: 'tv',
+};
+
+export const movieType = {
+  upcoming: 'upcoming',
+  popular: 'popular',
+  top_rated: 'top_rated',
+};
+
+export const tvType = {
+  popular: 'popular',
+  top_rated: 'top_rated',
+  on_the_air: 'on_the_air',
+};
+
+const movieApi = {
+  getMovieList: (type, params) => {
+    const url = `movie/${movieType[type]}`;
+
+    return axiosClient.get(url, params);
+  },
+
+  getTvList: (type, params) => {
+    const url = `tv/${tvType[type]}`;
+
+    return axiosClient.get(url, params);
+  },
+
+  getVideos: (cat, id) => {
+    const url = `${category[cat]}/${id}/videos`;
+
+    return axiosClient.get(url, { params: {} });
+  },
+
+  getDetails: (cat, id, params) => {
+    const url = `${category[cat]}/${id}`;
+
+    return axiosClient.get(url, params);
+  },
+
+  getCredits: (cat, id) => {
+    const url = `${category[cat]}/${id}/credits`;
+
+    return axiosClient.get(url, { params: {} });
+  },
+
+  getSimilar: (cat, id) => {
+    const url = `${category[cat]}/${id}/similar`;
+
+    return axiosClient.get(url, { params: {} });
+  },
+
+  search: (cat, params) => {
+    const url = `search/${category[cat]}`;
+
+    return axiosClient.get(url, params);
+  },
+};
+
+export default movieApi;
