1860853e03faa63cc692f654e65b000f3fc1c051
= <=>
2022-08-29 18:17:37 +0700

    fixed baseUrl

--- src/api/apiConfig.js
+++ src/api/apiConfig.js
@@ -1,5 +1,5 @@
 const apiConfig = {
-  baseUrl: process.env.REACT_APP_BASE_URL,
+  baseUrl: 'https://api.themoviedb.org/3/',
   apiKey: '7353f333c6ec9e7bee23f0a51ffc7291',
   originalImage: (imgPath) => `https://image.tmdb.org/t/p/original/${imgPath}`,
   width500Image: (imgPath) => `https://image.tmdb.org/t/p/w500/${imgPath}`,
