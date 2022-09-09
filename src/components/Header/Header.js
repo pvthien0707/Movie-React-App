d2709ebef64b6dbc330ff337135ddd77ef26cee3
= <=>
2022-09-09 19:11:49 +0700

    fixed route path home page

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,4 +1,4 @@
-import { useRef, useEffect, useState } from 'react';
+import { useRef, useEffect } from 'react';
 import { Link, useLocation } from 'react-router-dom';
 
 import classNames from 'classnames/bind';
